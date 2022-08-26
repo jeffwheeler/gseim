bc25c1929d18f5cd34552079bd3c63b25dcae8af
Jeff Wheeler <jeffwheeler@gmail.com>
2022-08-26 00:11:15 -0700

    Switch from Poetry to setuptools

--- gseim_grc/setup.py
+++ gseim_grc/setup.py
@@ -1,19 +1,20 @@
 from pathlib import Path
 
 import cmake_build_extension
+import setuptools
 
 ext_modules = [
     cmake_build_extension.CMakeExtension(
         name="gseim_solver",
         install_prefix="gseim_solver",
+        source_dir="src",
         expose_binaries=["bin/gseim_solver"],
         cmake_depends_on=["pybind11"],
     ),
 ]
 
-def build(setup_kwargs):
-    setup_kwargs.update({
-        "ext_modules": ext_modules,
-        "cmdclass": dict(build_ext=cmake_build_extension.BuildExtension),
-        "zip_safe": False,
-    })
+setuptools.setup(
+    ext_modules=ext_modules,
+    cmdclass=dict(build_ext=cmake_build_extension.BuildExtension),
+    zip_safe=False,
+)
--- gseim_grc/src/gseim_cpp_lib/main.cpp
+++ gseim_grc/src/gseim_cpp_lib/main.cpp
@@ -1,4 +1,4 @@
-#include "gseim_cpp_lib/gseim_solver.h"
+#include "gseim_solver.h"
 
 int main(int argc, char** argv) {
     return solve(argc, argv);
